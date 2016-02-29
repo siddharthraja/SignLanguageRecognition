c6a7b13c51f41f186dfe48a9f7a1ef6d6519c3cc
sraja <siddharthraja30@gmail.com>
2016-02-29 00:30:14 -0500

    server py code update for phrase switching, pausing, file handling

--- server/tcp-server.py
+++ server/tcp-server.py
@@ -14,63 +14,75 @@ s.listen(1)
 conn, addr = s.accept()
 print 'Connection address:', addr
 print 'Connection variable', conn
-count = 1 #........................................SET BACK TO 1 
+count = 19#........................................SET BACK TO 1 
 path = "./data_feed/"
+fullpath = ""
 f = None
 record = False
 start_msg = False
 delete_recording = False
+new_phrase = False
+folder = ""
 phrase = ""
 frame = 1
 file_name = ""
 
 while 1:
-    data = conn.recv(BUFFER_SIZE)
-    if not data: break
-    print "received data:", frame, len(data)
-    conn.send("ack "+str(frame))  # echo
-    frame = frame+1
+	data = conn.recv(BUFFER_SIZE)
+	if not data: break
+	print "received data:", frame, len(data)
+	conn.send("ack "+str(frame))  # echo
+	frame = frame+1
 
-    if data == "end":
-        record = False
-        phrase = ""
-        f.close()
-        print "End End End End End End End End"
+	if data == "end":
+		record = False
+		#phrase = ""
+		f.close()
+		print "End End End End End End End End"
 
-    if data=="exit": break
+	if data=="exit": break
 
-    if record:
-        f.write(data)
-        f.write("\n")
+	if record:
+		f.write(data)
+		f.write("\n")
 
-#    joints = data.split('|')        
-#    if len(joints)>0 and record:
-#       for joint in joints:
-#           #coord = joints(k).split(',')
-#           f.write(data)
+	if new_phrase:
+		new_phrase = False
+		print "New New New New New New New New"
+		phrase = data
+		folder = ""+phrase+"/"
+		directory = path+folder
+		if not os.path.exists(directory):
+			os.makedirs(directory)
+		count = 1
 
-    if start_msg:
-        record = True
-        start_msg = False
-        phrase = data
-        print "Start Start Start Start Start Start"
-        file_name = path+phrase+"_"+str(count)+".txt"
-        f = open(path+phrase+"_"+str(count)+".txt", "w")
-        count = count + 1
 
-    if delete_recording and count >= 1:
-        record = False
-        delete_recording = False
-        f.close()
-        count -= 1
-        os.remove(file_name)
-        phrase = ""
-        print "Deleted Deleted Deleted Deleted Deleted Deleted Deleted Deleted"
+	#start of each phrase sample
+	if start_msg:
+		record = True
+		start_msg = False
+		phrase = data
+		print "Start Start Start Start Start Start"
+		file_name = path+folder+phrase+"_"+str(count)+".txt"
+		f = open(path+folder+phrase+"_"+str(count)+".txt", "w")
+		count = count + 1
 
-    if data == "start":
-        start_msg = True
+	if delete_recording and count > 1:
+		record = False
+		delete_recording = False
+		f.close()
+		count -= 1
+		file_to_del = path+folder+phrase+"_"+str(count)+".txt"        
+		os.remove(file_to_del)
+		print "Deleted Deleted Deleted Deleted Deleted Deleted Deleted"
 
-    if data == "delete":
-        delete_recording = True
+	if data == "start":
+		start_msg = True
+
+	if data == "delete":
+		delete_recording = True
+		
+	if data == "new_phrase":
+		new_phrase = True
 
 conn.close()
